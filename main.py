91655b63a2aea42449df2cd1bee6a73bc4bb5ece
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2021-11-04 21:05:06 -0400

    misc commit

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from hash import HashMap
 
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-
     h = HashMap()
     h.add('Bob', '567-888')
     h.add('Ming', '293-6753')
@@ -17,7 +11,6 @@ if __name__ == '__main__':
     h.delete('Bob')
     h.print()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 
 
