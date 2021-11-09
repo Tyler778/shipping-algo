f7a18cf0471a0ce057f86776ce98b75def8b911e
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2021-11-09 00:04:36 -0500

    replaced HashTable and updated hash as well as created package class

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from hash import HashMap
 from initializer import loader
 import csv
+from WGUhash import ChainingHashTable
 
 h = HashMap()
-
+cht = ChainingHashTable()
 
 def distanceReader():
     with open("DistanceTable.csv") as dt:
@@ -18,17 +19,46 @@ def packageReader():
         data_read = [row for row in reader]
     x = 1
     while x < 41:
-        data_read[x].pop()
-        data_read[x].pop()
-        data_read[x].pop()
-        data_read[x].pop()
-        data_read[x].pop()
-        data_read[x].pop()
-        h.add(data_read[x].pop(0), data_read[x])
+        sliced = data_read[x][0:7]
+        pkg = Package(sliced[0],sliced[1],sliced[2],sliced[3],sliced[4],sliced[5],sliced[6])
+        cht.insert(pkg.getId(), pkg)
+
         x += 1
+
+class Package:
+    def __init__(self, id, address, city, state, zip, deliveryTime, weight):
+        self.id = id
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.deliveryTime = deliveryTime
+        self.weight = weight
+    def getId(self):
+        return self.id
+
+    def getAddress(self):
+        return self.address
+
+    def getCity(self):
+        return self.city
+
+    def getState(self):
+        return self.state
+
+    def getZip(self):
+        return self.zip
+
+    def getDeliveryTime(self):
+        return self.deliveryTime
+
+    def getWeight(self):
+        return self.weight
+
 
 
 
 if __name__ == '__main__':
     packageReader()
-    h.print()
+    print(cht.search(str(12)).getAddress())
+
