69ba653db246f95fefb61d0a44aa7ee5a039927b
Tyler Q <44939868+Tyler778@users.noreply.github.com>
2021-11-09 17:12:14 -0500

    manually loaded trucks with packages

--- Truck.py
+++
@@ -1,6 +0,0 @@
-from Package import Package
-
-class Truck:
-    def __init__(self, fool):
-        self.fool = fool
-
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from hash import HashMap
-from initializer import loader
 import csv
 from WGUhash import ChainingHashTable
 from Package import Package
+from Trucks import truck
 
 cht = ChainingHashTable()
 
@@ -13,8 +12,7 @@ def distanceReader():
         reader = csv.reader(dt, delimiter=",", quotechar='"')
         data_read = [row for row in reader]
         data_read.pop(0)
-        distanceData = data_read
-    return distanceData
+    return data_read
 
 def addressReader():
     with open("AddressTable.csv") as dt:
@@ -36,8 +34,8 @@ def packageReader():
         sliced = data_read[x][0:7]
         pkg = Package(sliced[0],sliced[1],sliced[2],sliced[3],sliced[4],sliced[5],sliced[6])
         cht.insert(pkg.getId(), pkg)
-        truckPkgs.append(pkg)
-
+        if int(pkg.getId()) % 5 == 4:
+            truckPkgs.append(pkg)
         x += 1
 
 def distanceBetween(address1, address2):
@@ -54,22 +52,55 @@ def distanceLookup(index1, index2):
     return distance
 
 def minDistanceFrom(fromAddress, truckPackages):
-    fromIndex = addressList.index(fromAddress)
-    packageAddresses = []
-    indexHold = []
+    minDistance = 100
     for i in truckPackages:
-        packageAddresses.append(i.getAddress())
-    for i in range(len(packageAddresses)):
-        indexHold = addressList.index(packageAddresses[i])
-    print(indexHold)
-    return packageAddresses
+        tempDistance = float(distanceBetween(fromAddress, i.getAddress()))
+        if tempDistance < minDistance:
+            minDistance = tempDistance
+
+
+    return minDistance
+
+def truckLoadPackages():
+    t1 = [1, 2, 4, 5, 7, 10, 11, 12, 17, 23, 24, 27, 29, 33, 35, 40]
+    #must be on t2 OR delivered to same spot
+    #3 18 36 38 MUST be t2
+    #13 14 15 16 19 20 MUST be on same truck
+    #39 delivered to same spot 13
+    #37 delivered to same spot 38
+    #34 delivered to same spot 15, 16
+    #21 delivered to same spot as 2
+    #30 AND 8 nearby other delivery locations
+    #15 9:00AM
+    #37, 13, 14, 16, 20, 34   ---  10:30AM
+    t2 = [3, 18, 36, 38, 13, 14, 15, 16, 19, 20, 39, 37, 34, 21, 30, 8]
+
+    #all delayed until 9:05 OR delivered to same spot
+    #26 delivered to same spot
+    #31 delivered to same spot
+    #22 nearby delivery
+    #6, 25, 31  --- 10:30AM
+    t3 = [6, 25, 26, 28, 31, 32, 9, 22]
+
+    for i in t1:
+        truckOne.load(cht.search(str(i)))
+    for i in t2:
+        truckTwo.load(cht.search(str(i)))
+    for i in t3:
+        truckThree.load(cht.search(str(i)))
+    return len(t1), len(t2), len(t3)
 
 
 addressList = addressReader()
 distanceList = distanceReader()
-
+truckOne = truck()
+truckTwo = truck()
+truckThree = truck()
 if __name__ == '__main__':
     packageReader()
     addressReader()
-    minDistanceFrom('3595 Main St', truckPkgs)
+    truckLoadPackages()
 
+    truckOne.getCountOfPackages()
+    truckTwo.getCountOfPackages()
+    truckThree.getCountOfPackages()
